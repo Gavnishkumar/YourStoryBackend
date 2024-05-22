6d70a8b960aef60baf30d3e85edb56cb60adb7ea
Gavnish kumar <gk991789@gmail.com>
2024-05-22 12:21:46 +0530

    authentication

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../Models/User');
-const generateToken = require('../config/genrateToken');
+const generateToken = require('../Config/genrateToken');
 const bcrypt= require('bcrypt');
 const jwt=require('jsonwebtoken');
 // const JWT_SECRET=process.env.JWT_SECRET;
