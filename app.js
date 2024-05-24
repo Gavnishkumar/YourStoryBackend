9b1503e9ae984b57078186d75aa42c79d8fd8cf7
aasn0119 <abhitrivedi119@gmail.com>
2024-05-24 12:18:00 +0530

    refactor: Update User controller and database configuration files

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express= require('express');
-const dotenv= require('dotenv');
-const UserRoute= require('./Route/UserRoute');
-const connectDB = require('./Config/db');
-
-const cors= require('cors');
-const app= express();
-app.use(cors())
-app.use(express.json());
+const express = require("express");
+const dotenv = require("dotenv");
+const UserRoute = require("./Route/UserRoute");
+const connectDB = require("./Config/db");
 dotenv.config();
+PORT = process.env.PORT;
+
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
 connectDB();
-app.use('/api/user',UserRoute);
-app.listen(process.env.PORT,(req,res)=>{
-    console.log(  `Server is running on ${7000}`)
-})
+app.use("/api/user", UserRoute);
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on ${PORT}`);
+});
