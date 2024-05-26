67838fd6539b28a7afd1031c467e9fc9b869bbd4
aasn0119 <abhitrivedi119@gmail.com>
2024-05-26 11:37:24 +0530

    refactor: Add authentication middleware to PostRoute endpoints

--- Route/StartupRoute.js
+++ Route/StartupRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const protect = require("../middleware/authmiddleware");
 const {
   createStartUp,
   updateStartUp,
@@ -8,9 +9,9 @@ const {
   fetchStartUpByID,
 } = require("../Controller/StartupController");
 
-router.route("/create").post(createStartUp);
-router.route("/update/:id").put(updateStartUp);
-router.route("/delete/:id").delete(deleteStartUp);
+router.route("/create").post(protect, createStartUp);
+router.route("/update/:id").put(protect, updateStartUp);
+router.route("/delete/:id").delete(protect, deleteStartUp);
 router.route("/all").get(fetchAllStartUps);
 router.route("/:id").get(fetchStartUpByID);
 
