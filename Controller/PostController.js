9521e53864daf906a2171f254b5ab93081df56c5
Gavnish kumar <gk991789@gmail.com>
2024-05-29 16:05:04 +0530

    create post fix

--- Controller/PostController.js
+++ Controller/PostController.js
@@ -8,7 +8,9 @@ const createPost = async (req, res) => {
   const { Image, Title, Description } = req.body;
   try {
     console.log(profileId)
-    const user = await Profile.findById(profileId).populate("User","Name Email Image");
+    const user = await Profile.findById(profileId)
+  .select("Name Email Image")
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
